b2011759b41983666d199ce462895854b75973fc
ShellBoyRD <82308473+ShellBoyRD@users.noreply.github.com>
2022-02-21 20:56:40 -0500

    Delete artix-pacman-improved.bash

--- artix-pacman-improved.bash
+++
@@ -1,7 +0,0 @@
-git clone https://github.com/ShellBoyRD/pacman-config-artix &&
-cd pacman-config-artix &&
-pacman -S artix-archlinux-support &&
-pacman-key --populate archlinux &&
-rm /etc/pacman.conf &&
-mv pacman.conf /etc/pacman.conf &&
-echo "all done pacman improved"
