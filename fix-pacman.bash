71645cf07f995d304b0801113e9f2a7c3e24293d
ShellBoyRD <82308473+ShellBoyRD@users.noreply.github.com>
2022-07-09 22:04:05 -0400

    Update fix-pacman.bash

--- fix-pacman.bash
+++ fix-pacman.bash
@@ -1,6 +1,7 @@
 pacman -Syyuu &&
 mv /etc/pacman.conf ~/pacman.conf.backup &&
 mv stage-1-temp-pacman.conf /etc/pacman.conf &&
+pacman -Syyuu
 pacman -S artix-archlinux-support &&
 pacman-key --populate archlinux &&
 rm /etc/pacman.conf &&
