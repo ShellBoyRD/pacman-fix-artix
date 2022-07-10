4c4451c1111bd06219e521293f4ec2a5eaae70bf
ShellBoyRD <82308473+ShellBoyRD@users.noreply.github.com>
2022-07-09 21:58:31 -0400

    Update fix-pacman.bash

--- fix-pacman.bash
+++ fix-pacman.bash
@@ -5,5 +5,5 @@ pacman -S artix-archlinux-support &&
 pacman-key --populate archlinux &&
 rm /etc/pacman.conf &&
 mv stege-2-temp-pacman.conf /etc/pacman.conf &&
-pacman -Syyuu
+pacman -Syyuu &&
 echo "all done pacman is fixed and improved, your old pacman.conf is backed up at ~/pacman.conf.backup in case you need it" 
